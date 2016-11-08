0b64b09a4935bbe2f4a210221bf51ee8a0d53e07
Joe Wilkinson <joe@Joes-MacBook-Pro.local>
2016-11-07 21:34:40 -0600

    finished app

--- src/main.js
+++ src/main.js
@@ -1,37 +1,114 @@
 import React, { component } from 'react';
 import {
+  AsyncStorage,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
 module.exports = React.createClass({
   getInitialState() {
     return ({
-      tasks: ['Clean Bathroom', 'Vacuum', 'Take Out Trash'],
+      tasks: [],
+      completedTasks: [],
       task: ''
     })
   },
 
+  componentWillMount() {
+    AsyncStorage.getItem('tasks')
+      .then((response) => {
+        this.setState({tasks: JSON.parse(response)})
+      });
+    AsyncStorage.getItem('completedTasks')
+      .then((response) => {
+        this.setState({completedTasks: JSON.parse(response)})
+      });
+  },
+
+  // Save states on all updates
+  componentDidUpdate() {
+    this.setStorage();
+  },
+
+  setStorage() {
+    // Save task list
+    AsyncStorage.setItem('tasks', JSON.stringify(this.state.tasks));
+    // Save completedTasks list
+    AsyncStorage.setItem('completedTasks', JSON.stringify(this.state.completedTasks));
+  },
+
   renderList(tasks) {
-    // TODO: render individual rows with the states tasks
     return (
-      tasks.map((task) => {
+      tasks.map((task, index) => {
         return (
           <View key={task} style={styles.task}>
             <Text>
               {task}
             </Text>
+            <TouchableOpacity
+              onPress={()=>this.completeTask(index)}
+            >
+              <Text>
+                &#10003;
+              </Text>
+            </TouchableOpacity>
           </View>
         )
       })
     )
   },
 
+  renderCompleted(tasks) {
+    return (
+      tasks.map((task, index) => {
+        return (
+          <View key={task} style={styles.task}>
+            <Text style={styles.completed}>
+              {task}
+            </Text>
+            <TouchableOpacity
+              onPress={()=>this.deleteTask(index)}
+            >
+              <Text>
+                &#10005;
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )
+      })
+    )
+  },
+
+  deleteTask(index) {
+    let completedTasks = this.state.completedTasks
+    completedTasks = completedTasks.slice(0, index).concat(completedTasks.slice(index+1));
+    // Updates component state to reflect removed task
+    this.setState({completedTasks});
+  },
+
+  completeTask(index) {
+    // Removes task from tasks list
+    let tasks = this.state.tasks;
+    tasks = tasks.slice(0, index).concat(tasks.slice(index + 1));
+    // Adds task to completedTasks list
+    let completedTasks = this.state.completedTasks;
+    completedTasks = completedTasks.concat([this.state.tasks[index]]);
+    // Updates component states to reflect list changes
+    this.setState({
+      tasks,
+      completedTasks
+    });
+  },
+
   addTask() {
     let tasks = this.state.tasks.concat([this.state.task]);
-    this.setState({tasks: tasks})
+    // Update component state to reflect added task
+    this.setState({tasks: tasks});
+    // TODO Remove text from input
   },
 
   render() {
@@ -45,11 +122,13 @@ module.exports = React.createClass({
           placeholder='Add a task...'
           onChangeText={(text) => {
             this.setState({task: text});
-            console.log(this.state.task);
           }}
           onEndEditing={()=>this.addTask()}
         />
-        {this.renderList(this.state.tasks)}
+        <ScrollView>
+          {this.renderList(this.state.tasks)}
+          {this.renderCompleted(this.state.completedTasks)}
+        </ScrollView>
       </View>
     )
   }
@@ -78,10 +157,17 @@ const styles = StyleSheet.create({
   },
 
   task: {
+    flexDirection: 'row',
     height: 60,
     borderBottomWidth: 1,
     borderColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20
+  },
+
+  completed: {
+    color: '#555',
+    textDecorationLine: 'line-through'
   }
 });
